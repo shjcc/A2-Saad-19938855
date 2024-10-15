0e2838f907a7ca0dfe250550587772c9ce956e76
Saad Hussain <19938855@students.ltu.edu.au>
2024-10-15 11:46:06 +1100

    added logic into the NewCompany.js, focusing on creating a new company in the database using method: POST

--- frontend/src/components/NewCompany.js
+++ frontend/src/components/NewCompany.js
@@ -4,6 +4,33 @@ function NewCompany(props){
     const {contact, companies, setCompanies} = props;
     const [company_name, setCompanyName] = useState('');
     const [company_address, setCompanyAddress] = useState('');
+
+    async function createCompany(e){
+        e.preventDefault();
+
+        const response = await fetch('http://localhost/api/contacts/' + contact.id + '/companies', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                company_name,
+                company_address
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.id) {
+            setCompanyName(...companies, data);
+        }
+
+        setCompanyName( '');
+        setCompanyAddress( '');
+
+
+    }
+
 }
 
 export default NewCompany;
