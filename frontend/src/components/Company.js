26538d85d1a16bea25106c3472d16c0292ec749c
Saad Hussain <19938855@students.ltu.edu.au>
2024-10-15 11:50:42 +1100

    updated code logic inside of Company.js to include a function for deleting a company which uses method 'DELETE'

--- frontend/src/components/Company.js
+++ frontend/src/components/Company.js
@@ -1,6 +1,18 @@
 function Company(props) {
     const {contact, company, companies, setCompanies} = props;
 
+    async function deleteCompany() {
+        const response = await fetch('http://localhost/api/contacts/' + contact.id + '/companies/' + company.id, {
+            method: 'DELETE',
+        });
+
+        let newCompanies = company.filter((p) => {
+            return p.id !== company.id;
+        });
+
+        setCompanies(newCompanies);
+    }
+
 }
 
 export default Company;
